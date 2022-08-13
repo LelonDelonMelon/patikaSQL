bdece7a75254f7c009fc758cd94d7d9491190d76
Burak Katı <burkinggs@gmail.com>
2022-08-13 05:34:31 +0300

    Add files via upload

---
+++ Odev7/Odev7.sql
@@ -0,0 +1,20 @@
+--1--
+SELECT rating, count(*) 
+FROM film 
+GROUP BY rating
+
+--2--
+SELECT replacement_cost, count(*) FROM film 
+GROUP BY replacement_cost
+HAVING count(replacement_cost) >50
+
+--3--
+
+SELECT store_id , COUNT(*) FROM customer
+GROUP BY store_id
+
+--4--
+Select  country_id, count(city) from city
+Group by country_id 
+ORDER BY COUNT(city_id) DESC
+Limit 1 ;
